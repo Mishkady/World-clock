578a58f7a72b7b313d8960c7a7abaddf1d5ca0ab
Mishkady <jessdanielova@gmail.com>
2023-02-27 20:55:13 +1100

    added link

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@
             <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
         </div>
     </div>
+    <a href="index.html">All cities</a>
         `
     }
 
